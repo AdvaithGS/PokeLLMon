1b9f0ff9a682559153bb232bd7a9a75bdb33525e
Advaith GS <advaith.gs4@gmail.com>
2025-07-11 12:33:34 +0530

    added gen_stats

--- url/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.urlShort, name="index"),
-    path("u/<str:slugs>", views.urlRedirect, name="redirect"),
-    path("stats/", views.url_stats, name = "stats")
-]
